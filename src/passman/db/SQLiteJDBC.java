e3f59c07a62410d5b685dd153eac0fceaecf702a
Andre Gomes <andre-t-gomes@telecom.pt>
2015-08-03 12:35:27 +0100

    table creation

--- src/passman/db/SQLiteJDBC.java
+++ src/passman/db/SQLiteJDBC.java
@@ -12,16 +12,29 @@ import java.sql.*;
  * @author P057736
  */
 public class SQLiteJDBC {
-    public void createConnection(){
-        Connection c = null;
-        
+    public void createConnection(){        
         try{
             Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:passman.db");
+            Connection c = DriverManager.getConnection("jdbc:sqlite:passman.db");
+            
+            ResultSet rs = c.getMetaData().getTables(null, null, "passwords", null);
+            // Create table
+            if(!rs.next()){
+                try (Statement stmt = c.createStatement()) {
+                    String sql = "CREATE TABLE passwords2 " +
+                            "(ID INT PRIMARY KEY NOT NULL," +
+                            "LABEL TEXT NOT NULL, " +
+                            "USERNAME CHAR(50) NOT NULL, " +
+                            "PASSWORD CHAR(50) NOT NULL, " +
+                            "COMMENT CHAR(250))";
+                    
+                    stmt.executeUpdate(sql);
+                }
+                c.close();
+            }
         } catch (ClassNotFoundException | SQLException e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
-        System.out.println("Opened database successfully");
     }
 }
